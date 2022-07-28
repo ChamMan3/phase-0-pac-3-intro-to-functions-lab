0903bd15f0f16fda5c32b38f21f9a43708d1e939
lukechamley <lkchamley@gmail.com>
2022-07-28 14:16:04 -0600

    commit tree 3459910296a164294790eec98c06bf7561ea5ed5

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+return string.toUpperCase()
+}
+function whisper(string) {
+return string.toLowerCase()
+}
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(string){
+   if (console.log(string = whisper("I can\'t hear you!")));
+   if (console.log(string = shout("Yes Indeed!")));
+   if (string==="Let\'s have dinner together") return ("I would love to!")
+ 
+}
+    
+
